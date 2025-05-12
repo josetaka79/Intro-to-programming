895bb59645515935bae11fe89220bd808c0c11f2
josetaka79 <josetaka79@gmail.com>
2025-05-12 15:50:33 +0200

    Add files via upload

---
+++ Chapter 4/Page 76/scoreKeeper.js
@@ -0,0 +1,18 @@
+//1. Object with initial scores.
+const scores= { Aaron: 0, Edith: 0, Ncha:0, Khan:0};
+//2. Increase players scores.
+// lets assume Khan scores 12 points.
+scores.Khan += 12;
+
+// lets assume Aaron scores 10 points.
+scores.Aaron +=10;
+
+//lets assume Edith scores 7 points.
+scores.Edith +=7;
+
+// lets assume Ncha scores 3 points
+scores.Ncha += 3;
+
+console.log("Scores:", scores);
+
+
