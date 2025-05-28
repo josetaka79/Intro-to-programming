755b57a7664cfd953d096aed353a769ddd8a34c6
josetaka79 <josetaka79@gmail.com>
2025-05-28 20:22:36 +0200

    Add files via upload

---
+++ Book 2/Quiz/Quiz.js
@@ -0,0 +1,10 @@
+var choice = prompt("Do you want cake, cheese, or pork for dessert?");
+if (choice === "cake") {
+  console.log("serve cake");
+} else if (choice === "cheese") {
+  console.log("serve cheese");
+} else if (choice === "pork") {
+  console.log("serve pork");
+} else {
+  console.log("nothing was chosen");
+}
